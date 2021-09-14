5c0a92c78a88865a0360522e6b3b76c4e5f043bc
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2021-09-14 17:22:14 +0200

    fixed a bug that caused pymusco to crash if the first page of the stub doesn't contain a track (eg instruments list)

--- src/pymusco/main.py
+++ src/pymusco/main.py
@@ -143,7 +143,6 @@ class StubContents(PdfContents):
         current_track_num_pages = 0
         date_as_string = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
         for page_index in range(1, len(self.image_file_paths) + 1):
-
             if self.toc:
                 page_tracks = self.toc.get_tracks_for_page(page_index)
                 if len(page_tracks) > 0:
@@ -154,7 +153,7 @@ class StubContents(PdfContents):
                     current_track_num_pages = toc.get_tracks_last_page_index(current_tracks, len(self.image_file_paths)) - toc.get_tracks_first_page_index(current_tracks) + 1
                     self.page_to_section[page_index] = '/'.join([track.id for track in current_tracks])
 
-            self.page_footers[page_index] = r'%s on %s - page %d/%d : %s - page %d/%d' % (self.title, date_as_string, page_index, len(self.image_file_paths), '/'.join([track.id for track in current_tracks]), current_track_page_number, current_track_num_pages)
+                    self.page_footers[page_index] = r'%s on %s - page %d/%d : %s - page %d/%d' % (self.title, date_as_string, page_index, len(self.image_file_paths), '/'.join([track.id for track in current_tracks]), current_track_page_number, current_track_num_pages)
 
             current_track_page_number += 1
 
